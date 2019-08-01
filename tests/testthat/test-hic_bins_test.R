a7cc98121f9ef90130561f2405f0f6e3b5e05665
areyesq89 <alejandro.reyes.ds@gmail.com>
2019-08-01 16:00:30 -0500

    Added goal of converting bedpe to GI and back

---
+++ tests/testthat/test-hic_bins_test.R
@@ -0,0 +1,13 @@
+test_that("Import and export work for bedpe", {
+  library(GenomicAlignments)
+  library(GenomicInteractions)
+  hic_file <- system.file("extdata", "Seitan2013_WT_100kb_interactions.txt",
+                          package="GenomicInteractions")
+  hic_data <- makeGenomicInteractionsFromFile(hic_file,
+                                              type="homer",
+                                              experiment_name = "HiC 100kb",
+                                              description = "HiC 100kb resolution")
+  gi_export( hic_data, format="bedpe", file="hic_data.bedpe" )
+  hic_data_read <- gi_import("hic_data.bedpe")
+  expect_equal( hic_data, hic_data_read )
+} )
