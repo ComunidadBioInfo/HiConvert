9904174bdf7dc8d970765fc0975dc9b920949564
areyesq89 <alejandro.reyes.ds@gmail.com>
2019-08-01 15:46:08 -0500

    Added template to convert

---
+++ R/hic_bins.R
@@ -0,0 +1,12 @@
+
+library(GenomicAlignments)
+library(GenomicInteractions)
+hic_file <- system.file("extdata", "Seitan2013_WT_100kb_interactions.txt",
+                        package="GenomicInteractions")
+
+hic_data <- makeGenomicInteractionsFromFile(hic_file,
+                                            type="homer",
+                                            experiment_name = "HiC 100kb",
+                                            description = "HiC 100kb resolution")
+
+hic_data
