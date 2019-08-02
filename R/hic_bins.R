3138726932422a9307e82db6eae51b48df640fcb
CarlosBarronG <37635832+CarlosBarronG@users.noreply.github.com>
2019-08-02 10:31:31 -0500

    hice las funciones de import y export en bins

--- R/hic_bins.R
+++ R/hic_bins.R
@@ -1,16 +1,34 @@
 
-library(GenomicAlignments)
-library(GenomicInteractions)
-hic_file <- system.file("extdata", "Seitan2013_WT_100kb_interactions.txt",
-                        package="GenomicInteractions")
-hic_data <- makeGenomicInteractionsFromFile(hic_file,
-                                            type="homer",
-                                            experiment_name = "HiC 100kb",
-                                            description = "HiC 100kb resolution")
+gi_export <- function( x, format="bedpe", file){
+  a<-data.frame(
+    Chrom1=x@regions[x@anchor1],
+    Chrom2=x@regions[x@anchor2],
+    name=x@elementMetadata@listData[["InteractionID"]],
+    score=x@elementMetadata@listData[["counts"]],
+    strand1=x@elementMetadata@listData[["strand.1."]],
+    strand2=x@elementMetadata@listData[["strand.2."]])
+  a <- dplyr::select( a,
+                      -Chrom1.width,
+                      -Chrom1.strand,
+                      -Chrom2.width,
+                      -Chrom2.strand,
+                      -Chrom2.strand) %>%
+    dplyr::rename(
+      chrom1=Chrom1.seqnames,
+      start1=Chrom1.start,
+      end1=Chrom1.end,
+      chrom2=Chrom2.seqnames,
+      start2=Chrom2.start,
+      end2=Chrom2.end)
+  if(format=="bedpe"){
+    write.table(a,file=paste0(file,".bedpe"),
+                sep = "\t",
+                quote = FALSE,
+                row.names = FALSE,
+                col.names=FALSE)
+  }else stop("the current version only supports bedpe files")
+}
 
-hic_data
-x <- hic_data
-
-gi_export - function( x, format="bedpe", file){
-
+gi_import<-function(filename,type){
+  makeGenomicInteractionsFromFile(fn=filename,type = type)
 }
