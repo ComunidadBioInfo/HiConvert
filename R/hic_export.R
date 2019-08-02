73ecd0fda0615c6d64eac22b4deb256e84f87279
Joselyn Chavez <joselynchavezf@gmail.com>
2019-08-02 11:49:52 -0500

    export bam files added

--- R/hic_export.R
+++ R/hic_export.R
@@ -1,29 +1,38 @@
 #' Exports HiC file
 #'
-#' Exports an GAlignment object as bedpe file
+#' Exports an GAlignment object to a file with the specified format
 #'
 #' @param x A GAlignmentPairs object
 #' @param file path or name of the output file
+#' @param format the file format to export
 #'
 #' @return A sequence file with the specified format
 #' @export
 #'
 #' @examples
 #' x <- hic_import("example.bam")
-#' hic_export(x, "example.bedpe")
-hic_export <- function(x, file) {
+#' hic_export(x, "example.bedpe", format = "bedpe")
+#' hic_export(x, "example.bam", format = "bam")
+
+hic_export <- function(x, file, format) {
   gfirst <- granges(x@first)
   gsecond <- granges(x@last)
-  output_bedpe <- data.frame(chrom1 = seqnames(gfirst),
-                             start1 = start(gfirst),
-                             end1 = end(gfirst),
-                             chrom2 = seqnames(gsecond),
-                             start2 = start(gsecond),
-                             end2 = end(gsecond),
-                             name = names(x) ,
-                             score = ".",
-                             strand1 = strand(gfirst),
-                             strand2 = strand(gsecond),
-                             others = ".")
-  write.table(output_bedpe, file = file, row.names = FALSE, col.names = FALSE)
+  if (format == "bedpe") {
+    output_bedpe <- data.frame(chrom1 = seqnames(gfirst),
+                               start1 = start(gfirst),
+                               end1 = end(gfirst),
+                               chrom2 = seqnames(gsecond),
+                               start2 = start(gsecond),
+                               end2 = end(gsecond),
+                               name = names(x) ,
+                               score = ".",
+                               strand1 = strand(gfirst),
+                               strand2 = strand(gsecond),
+                               others = ".")
+    write.table(output_bedpe, file = file, row.names = FALSE, col.names = FALSE)
+  }
+  if (format == "bam") {
+    export(x, file, format = format)
+
+  }
 }
