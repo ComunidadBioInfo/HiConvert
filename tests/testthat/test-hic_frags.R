2fcd22a6dc332297bc0067ff53d777a95847234f
areyesq89 <alejandro.reyes.ds@gmail.com>
2019-08-02 10:20:19 -0500

    Added vignette

--- tests/testthat/test-hic_frags.R
+++ tests/testthat/test-hic_frags.R
@@ -1,5 +1,5 @@
 test_that("hicfrags works", {
   in_file <- system.file( "extdata/bam/SRR400264_subset.bam",
                          package="HiConvert", mustWork=TRUE )
-  hic_import(in_file)
+  imported <- hic_import(in_file)
 } )
